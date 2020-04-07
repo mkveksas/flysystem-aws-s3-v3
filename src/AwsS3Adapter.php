820e4e390b79c25c88e5d00831b18d6eb80f7c85
Martynas Kveksas <obalitas@gmail.com>
2020-04-07 15:39:05 +0100

    Update AwsS3Adapter.php

--- src/AwsS3Adapter.php
+++ src/AwsS3Adapter.php
@@ -597,7 +597,7 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles
                 // The first 10000 bytes of the file is hopefully enough to guess the mime type,
                 // otherwise if we send the whole file, we might run out of memory depending on 
                 // the file size.
-                $options['ContentType'] = Util::guessMimeType($path, substr($body, 0, 10000));
+                $options['ContentType'] = Util::guessMimeType($path, $body);
             }
 
             if ( ! isset($options['ContentLength'])) {
